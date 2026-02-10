f616d68e1822e7f90007ca8beb137421c7d2bd89
Genesis <geespino2001@gmail.com>
2026-02-10 14:23:51 -0500

    Actualizacion de las siguientes paginas src/App.js src/BlogPage.js src/BlogPost.js src/HomePage.js src/LoginPage.js src/LogoutPage.js src/Menu.js src/NotFoundPage.js src/ProfilePage.js src/auth.js src/blogdata.js

---
+++ src/blogdata.js
@@ -0,0 +1,22 @@
+
+const blogdata = [];
+blogdata.push({
+    title: '¿Que es React?',
+    slug: 'que-es-react',
+    content: 'React es el mejor framework de JavaScript.',
+    author: 'gespino',
+});
+blogdata.push({
+    title: '¿Que es Vue?',
+    slug: 'que-es-Vue',
+    content: 'Vue es el mejor framework de JavaScript.',
+    author: 'randy',
+});
+blogdata.push({
+    title: '¿Que es Angular?',
+    slug: 'que-es-Angular',
+    content: 'Angular es el mejor framework de JavaScript.',
+    author: 'jrami',
+});
+
+export { blogdata };
